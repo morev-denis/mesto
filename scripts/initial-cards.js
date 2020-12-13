1de0c186bf05b9a05168e99f3b8478c0c285768b
Denis Morev <morev.denis@gmail.com>
2020-12-14 00:09:46 +0300

    исправить замечания ревьюера

---
+++ scripts/initial-cards.js
@@ -0,0 +1,27 @@
+// Массив для вывода карточек при загрузке
+const initialCards = [
+  {
+    name: 'Карачаево-Черкессия',
+    link: 'https://cdn.pixabay.com/photo/2018/06/13/09/51/russia-3472330_960_720.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: 'https://cdn.pixabay.com/photo/2020/06/14/22/27/the-caucasus-5299593_960_720.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: 'https://cdn.pixabay.com/photo/2020/02/15/12/57/dombay-4850907_960_720.jpg'
+  },
+  {
+    name: 'Псков',
+    link: 'https://cdn.pixabay.com/photo/2017/09/04/08/46/pskov-2713252_960_720.jpg'
+  },
+  {
+    name: 'Ярославль',
+    link: 'https://cdn.pixabay.com/photo/2016/09/27/19/45/russia-1699151_960_720.jpg'
+  },
+  {
+    name: 'Рыбинск',
+    link: 'https://cdn.pixabay.com/photo/2015/08/13/13/43/rybinsk-887045_960_720.jpg'
+  }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const root = document.querySelector('.root');
+
 const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileJob = profile.querySelector('.profile__job');
@@ -17,56 +19,25 @@ const popupCardAddFormName = popupCardAdd.querySelector('.popup__input_field_pla
 const popupCardAddFormLink = popupCardAdd.querySelector('.popup__input_field_link');
 
 const popupImageFullsize = document.querySelector('.popup_feat_image-fullsize');
+const popupImageFullsizeImg = popupImageFullsize.querySelector('.popup__image-fullsize');
+const popupImageFullsizeHeading = popupImageFullsize.querySelector('.popup__heading_feat_image-fullsize');
+const popupImageFullsizeButtonClose = popupImageFullsize.querySelector('.popup__button_action_close');
 
 const elementTemplate = document.querySelector('#element').content; // Шаблон карточки
 const elementsGrid = document.querySelector('.elements__grid'); // Список для вставки карточек
 
-// Массив для вывода карточек при загрузке
-const initialCards = [
-  {
-    name: 'Карачаево-Черкессия',
-    link: './images/elements/__element/elements__element_kirill-pershin-1088404-unsplash.jpg',
-    alt: 'Крепость в Карачаево-Черкессии'
-  },
-  {
-    name: 'Гора Эльбрус',
-    link: './images/elements/__element/elements__element_kirill-pershin-1404681-unsplash.jpg',
-    alt: 'Вершины Эльбруса'
-  },
-  {
-    name: 'Домбай',
-    link: './images/elements/__element/elements__element_kirill-pershin-1556355-unsplash.jpg',
-    alt: 'Горы Домбая'
-  },
-  {
-    name: 'Псков',
-    link: './images/elements/__element/elements__element_fortress-4490460_1280.jpg',
-    alt: 'Вид на крепость Пскова'
-  },
-  {
-    name: 'Ярославль',
-    link: './images/elements/__element/elements__element_river-2615647_1280.jpg',
-    alt: 'Вид на стрелку Ярославля'
-  },
-  {
-    name: 'Рыбинск',
-    link: './images/elements/__element/elements__element_rybinsk-887045_1280.jpg',
-    alt: 'Вид на набережную Рыбинска'
-  }
-];
-
 // Функция запрета вертикального скролла
 function disableScrollY() {
-  document.body.style.overflowY = 'hidden';
+  root.classList.add('root_scroll_disable');
 }
 
 // Функция разрешения вертикального скролла
 function enableScrollY() {
-  document.body.style.overflowY = 'auto'; //
+  root.classList.remove('root_scroll_disable');
 }
 
 // Функция отображения попап
-function displayPopup(targetPopup) {
+function showPopup(targetPopup) {
   targetPopup.classList.add('popup_opened');
 }
 
@@ -76,85 +47,95 @@ function hidePopup(targetPopup) {
 }
 
 // Функция открытия попап редактирования профиля
-function openPopupProfileEdit() {
-  displayPopup(popupProfileEdit);
+function handleProfileEditOpen() {
+  showPopup(popupProfileEdit);
   popupProfileEditFormName.value = profileName.textContent;
   popupProfileEditFormJob.value = profileJob.textContent;
   disableScrollY();
 }
 
 // Функция закрытия попап редактирования профиля
-function closePopupProfileEdit() {
+function handleProfileEditClose() {
   popupProfileEdit.classList.remove('popup_opened');
   enableScrollY();
 }
 
 // Функция изменения Имени и О себе через попап
-function popupProfileEditFormSubmitHandler(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault(); // Отменить стандартную отправку формы
   profileName.textContent = popupProfileEditFormName.value; // Присвоить Имени на HTML странице Имя из формы
   profileJob.textContent = popupProfileEditFormJob.value;  // Присвоить О себе на HTML странице О себе из формы
-  closePopupProfileEdit(); // Закрыть попап
+  handleProfileEditClose(); // Закрыть попап
 }
 
 // Функция открытия попап добавления нового места
-function openPopupCardAdd() {
-  displayPopup(popupCardAdd);
+function handelCardAddOpen() {
+  showPopup(popupCardAdd);
   disableScrollY();
 }
 
 // Функция закрытия попап добавления нового места
-function closePopupCardAdd() {
+function handleCardAddClose() {
   hidePopup(popupCardAdd);
   enableScrollY();
 }
 
+// Функция закрытия попап с полноразмерной картинкой
+function handleImageFullsizeClose() {
+  hidePopup(popupImageFullsize);
+  enableScrollY();
+}
+
 // Функция формирования содержимого карточки
-function generateCard(link, alt, name) {
+function createCard(link, name) {
   const element = elementTemplate.cloneNode(true);
-  element.querySelector('.element__image').src = link;
-  element.querySelector('.element__image').alt = alt;
+  const elementImage = element.querySelector('.element__image');
+  elementImage.src = link;
+  elementImage.alt = name;
   element.querySelector('.element__heading').textContent = name;
   element.querySelector('.element__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__like_active');
   });
-  element.querySelector('.element__button').addEventListener('click', function (evt) {
+  element.querySelector('.element__delete').addEventListener('click', function (evt) {
     evt.target.closest('.element').remove();
   });
 
-  element.querySelector('.element__image').addEventListener('click', function (evt) {
-    popupImageFullsize.querySelector('.popup__image-fullsize').src = evt.target.src;
-    popupImageFullsize.querySelector('.popup__heading_feat_image-fullsize').textContent = evt.target.nextElementSibling.querySelector('.element__heading').textContent;
-    displayPopup(popupImageFullsize);
+  elementImage.addEventListener('click', function () {
+    popupImageFullsizeImg.src = link;
+    popupImageFullsizeImg.alt = name;
+    popupImageFullsizeHeading.textContent = name;
+    showPopup(popupImageFullsize);
     disableScrollY();
   });
 
-  popupImageFullsize.querySelector('.popup__button_action_close').addEventListener('click', function (evt) {
-    hidePopup(popupImageFullsize);
-    enableScrollY();
-  });
+  return element;
+}
+
+// Функция добавления карточки
+function addCard(element) {
   elementsGrid.prepend(element);
 }
 
 // Функция добавления нового места
-function popupCardAddFormSubmitHandler(evt) {
+function handleCardFormSubmit(evt) {
   evt.preventDefault(); // Отменить стандартную отправку формы
-  generateCard(popupCardAddFormLink.value, popupCardAddFormName.value, popupCardAddFormName.value);
-  closePopupCardAdd(); // Закрыть попап
-  popupCardAddFormLink.value = ''; // Очистить поле ввода Ссылка на картинку
-  popupCardAddFormName.value = ''; // Очистить поле ввода Название
+  addCard(createCard(popupCardAddFormLink.value, popupCardAddFormName.value));
+  handleCardAddClose(); // Закрыть попап
+  popupCardAddForm.reset(); // Очистить поля ввода
 }
 
 // Вывод карточек из массива при загрузке
 initialCards.forEach(function(item) {
-  generateCard(item.link, item.alt, item.name);
+  addCard(createCard(item.link, item.name));
 });
 
-profileButtonEdit.addEventListener('click', openPopupProfileEdit); // Прикрепить обработчик к кнопке редактирования профиля
-profileButtonAdd.addEventListener('click', openPopupCardAdd); // Прикрепить обработчик к кнопке добавления нового места
+profileButtonEdit.addEventListener('click', handleProfileEditOpen); // Прикрепить обработчик к кнопке редактирования профиля
+profileButtonAdd.addEventListener('click', handelCardAddOpen); // Прикрепить обработчик к кнопке добавления нового места
 
-popupProfileEditButtonClose.addEventListener('click', closePopupProfileEdit); // Прикрепить обработчик к кнопке закрытия попап редактирования профиля
-popupProfileEditForm.addEventListener('submit', popupProfileEditFormSubmitHandler); // Прикрепить обработчик к форме редактирования профиля
+popupProfileEditButtonClose.addEventListener('click', handleProfileEditClose); // Прикрепить обработчик к кнопке закрытия попап редактирования профиля
+popupProfileEditForm.addEventListener('submit', handleProfileFormSubmit); // Прикрепить обработчик к форме редактирования профиля
 
-popupCardAddButtonClose.addEventListener('click', closePopupCardAdd); // Прикрепить обработчик к кнопке закрытия попап добавления нового места
-popupCardAddForm.addEventListener('submit', popupCardAddFormSubmitHandler); // Прикрепить обработчик к форме добавления нового места
+popupCardAddButtonClose.addEventListener('click', handleCardAddClose); // Прикрепить обработчик к кнопке закрытия попап добавления нового места
+popupCardAddForm.addEventListener('submit', handleCardFormSubmit); // Прикрепить обработчик к форме добавления нового места
+
+popupImageFullsizeButtonClose.addEventListener('click', handleImageFullsizeClose); // Прикрепить обработчик к кнопке закрытия попап полноразмерной картинки
